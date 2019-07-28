e9af7ed5b8db62081d7301e70d824e699757fead
carlosmartin moscosocornejo <carlosmartinmoscosocornejo@gmail.com>
2019-07-28 18:06:03 -0300

    hola mundo

---
+++ Serpentarios/Serpentarios/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Serpentarios.Models
+{
+  public class ErrorViewModel
+  {
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
